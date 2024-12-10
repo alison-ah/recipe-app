a7a7477802dab321727f65432b9248874d86005c
Alison <alison@andersonholland.com>
2024-12-10 15:48:27 -0600

    Full recipe view added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from "react";
 import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
+import RecipeFull from "./components/RecipeFull";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([])
+  const [selectedRecipe, setSelectedRecipe] = useState(null)
 
 
   useEffect(() => {
@@ -24,14 +26,25 @@ function App() {
     fetchAllRecipes();
   }, []);
 
+  const handleSelectRecipe = (recipe) => {
+    setSelectedRecipe(recipe)
+  }
+
+  const handleUnselectRecipe = () => {
+    setSelectedRecipe(null)
+  }
+
   return (
     <div className='recipe-app'>
       <Header />
-      <div className="recipe-list">
-      {recipes.map((recipe) => (
-        <RecipeExcerpt key={recipe.id} recipe={recipe}/>
-      ))}
-      </div>
+      {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
+      {!selectedRecipe && (
+        <div className='recipe-list'>
+          {recipes.map((recipe) => (
+            <RecipeExcerpt key={recipe.id} recipe={recipe} handleSelectRecipe={handleSelectRecipe} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
