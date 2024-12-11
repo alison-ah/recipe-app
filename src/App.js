8ffd9c81c073ee6edb59bdbb9d6aef3197283905
Alison <alison@andersonholland.com>
2024-12-11 09:11:57 -0600

    Added front-end new recipe form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import Header from "./components/Header";
 import RecipeExcerpt from "./components/RecipeExcerpt";
 import RecipeFull from "./components/RecipeFull";
 import "./App.css";
+import NewRecipeForm from "./components/NewRecipeForm";
 
 function App() {
   const [recipes, setRecipes] = useState([])
   const [selectedRecipe, setSelectedRecipe] = useState(null)
-
+  const [newRecipe, setNewRecipe] = useState({
+      title: "",
+      ingredients: "",
+      instructions: "",
+      servings: 1, // conservative default
+      description: "",
+      image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+});
+const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -26,17 +35,34 @@ function App() {
     fetchAllRecipes();
   }, []);
 
+  const onUpdateForm = (e) => {
+    const { name, value } =e.target;
+    setNewRecipe({...newRecipe, [name]: value});
+  }
+
   const handleSelectRecipe = (recipe) => {
     setSelectedRecipe(recipe)
-  }
+  };
 
   const handleUnselectRecipe = () => {
     setSelectedRecipe(null)
-  }
+  };
+
+  const hideRecipeForm = () => {
+    setShowNewRecipeForm(false);
+  };
+
+  const showRecipeForm = () => {
+    setShowNewRecipeForm(true);
+    setSelectedRecipe(null);
+  };
 
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm}/>
+      {showNewRecipeForm && (
+        <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm}/>
+        )}
       {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
       {!selectedRecipe && (
         <div className='recipe-list'>
