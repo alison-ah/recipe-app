2719e2ca631d8f181df8e3a78948b38b94bca2d9
Alison <alison@andersonholland.com>
2024-12-11 13:01:17 -0600

    Truncate of recipe excerpt description added

--- src/components/RecipeExcerpt.js
+++ src/components/RecipeExcerpt.js
@@ -1,19 +1,19 @@
 import React from "react";
+import { truncateText } from "../helpers/utils";
 
-const RecipeExcerpt = ({recipe, handleSelectRecipe}) => {
-    return (
-        <article className="recipe-card">
-            <figure>
-                <img src={recipe.image_url} alt={recipe.title} />
-            </figure>
-            <h2>
-             {recipe.title}
-            </h2>
-            <p className ="flex-spacing">Description: {recipe.description}</p>
-            <button onClick={() => handleSelectRecipe(recipe)}>View</button>
-
-        </article>
-    );
+const RecipeExcerpt = ({ recipe, handleSelectRecipe }) => {
+  return (
+    <article className="recipe-card">
+      <figure>
+        <img src={recipe.image_url} alt={recipe.title} />
+      </figure>
+      <h2>{recipe.title}</h2>
+      <p className="flex-spacing">
+        Description: {truncateText(recipe.description, 25)}
+      </p>
+      <button onClick={() => handleSelectRecipe(recipe)}>View</button>
+    </article>
+  );
 };
 
-export default RecipeExcerpt
+export default RecipeExcerpt;
